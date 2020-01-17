1202ddd9c938859973a351c983fb014568f6ca90
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-01-16 21:47:26 -0500

    Updates

---
+++ precache-manifest.ccb659292a7f426ba2d8d9fad03e9d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5f56636b5a1daa29a839bb16d84a96",
+    "url": "/JavaScriptCalculator/index.html"
+  },
+  {
+    "revision": "33caf25b3685912817c7",
+    "url": "/JavaScriptCalculator/static/css/main.d882b92c.chunk.css"
+  },
+  {
+    "revision": "421bd4fee3f0275f270b",
+    "url": "/JavaScriptCalculator/static/js/2.ae603ada.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/JavaScriptCalculator/static/js/2.ae603ada.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33caf25b3685912817c7",
+    "url": "/JavaScriptCalculator/static/js/main.18fc3fc6.chunk.js"
+  },
+  {
+    "revision": "10f6ec9f9dbe3ab23969",
+    "url": "/JavaScriptCalculator/static/js/runtime-main.af7213d2.js"
+  }
+]);
